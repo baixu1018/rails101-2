bb0e42a4899d641c82de4f046b4ed369b3f38bc3
baixu1018 <baixu.1018@msn.cn>
2017-04-04 15:59:40 +0800

    implement my posts edit and deleted

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,23 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to account_posts_path
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path
+  end
+
 
   private
 
